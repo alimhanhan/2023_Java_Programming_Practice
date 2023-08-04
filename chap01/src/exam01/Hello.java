68ab2814aa0abde24a00ed7bcac8fde1c699d013
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-08-04 11:04:43 +0900

    Add files via upload

---
+++ chap01/src/exam01/Hello.java
@@ -0,0 +1,11 @@
+package exam01;
+//2023 3월 15일 자바 프로그래밍 수업
+
+public class Hello 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello JAVA!");
+		System.out.println("Who are you??");
+	}
+}
