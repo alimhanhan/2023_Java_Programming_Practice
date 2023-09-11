c31239e9deccd1d261ea797c1260a0058fded4ce
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-09-11 14:35:14 +0900

    Add files via upload

---
+++ Reference/매개변수의 다형성/interface/DriverExample.java
@@ -0,0 +1,12 @@
+
+public class DriverExample {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.drive(bus);
+		driver.drive(taxi);
+	}
+}
